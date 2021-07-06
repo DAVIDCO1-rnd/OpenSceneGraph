73ffa5428938db04f15c084981bc0d28c976dcb2
David Cohn <DAVIDCO1@rnd-hub.com>
2021-07-07 00:34:55 +0300

    Updated examples/osganalysis/osganalysis.cpp

--- examples/osganalysis/osganalysis.cpp
+++ examples/osganalysis/osganalysis.cpp
@@ -495,7 +495,7 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-////page126
+//page126
 
 //#include <osg/PolygonMode>
 //#include <osg/MatrixTransform>
@@ -504,7 +504,7 @@
 //
 //int main(int argc, char** argv)
 //{
-//	osg::ref_ptr<osg::Node> model = osgDB::readNodeFile("cessna.osg");
+//	osg::ref_ptr<osg::Node> model = osgDB::readNodeFile("avatar.osg");
 //
 //	osg::ref_ptr<osg::MatrixTransform> transformation1 = new osg::MatrixTransform;
 //	transformation1->setMatrix(osg::Matrix::translate(-25.0f, 0.0f, 0.0f));
@@ -512,11 +512,16 @@
 //
 //	osg::ref_ptr<osg::MatrixTransform> transformation2 = new osg::MatrixTransform;
 //	transformation2->setMatrix(osg::Matrix::translate(25.0f, 0.0f, 0.0f));
+//	transformation2->setMatrix(osg::Matrix::rotate(0.5, osg::Vec3d(1.0, 0.0, 0.0)));
 //	transformation2->addChild(model.get());
 //
-//	osg::ref_ptr<osg::PolygonMode> polygonMode = new osg::PolygonMode;
-//	polygonMode->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
-//	transformation1->getOrCreateStateSet()->setAttribute(polygonMode.get());
+//	osg::ref_ptr<osg::PolygonMode> polygonMode1 = new osg::PolygonMode;
+//	polygonMode1->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
+//	transformation1->getOrCreateStateSet()->setAttribute(polygonMode1.get());
+//
+//	osg::ref_ptr<osg::PolygonMode> polygonMode2 = new osg::PolygonMode;
+//	polygonMode2->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::FILL);
+//	transformation2->getOrCreateStateSet()->setAttribute(polygonMode2.get());
 //
 //	osg::ref_ptr<osg::Group> root = new osg::Group;
 //	root->addChild(transformation1.get());
@@ -530,33 +535,35 @@
 
 //page 129
 
-//#include <osg/PolygonMode>
-//#include <osg/MatrixTransform>
-//#include <osgDB/ReadFile>
-//#include <osgViewer/Viewer>
-//
-//int main(int argc, char** argv)
-//{
-//	osg::ref_ptr<osg::Node> model = osgDB::readNodeFile("glider.osg");
-//	osg::ref_ptr<osg::MatrixTransform> transformation1 = new osg::MatrixTransform;
-//	transformation1->setMatrix(osg::Matrix::translate(-0.5f, 0.0f, 0.0f));
-//	transformation1->addChild(model.get());
-//	osg::ref_ptr<osg::MatrixTransform> transformation2 = new osg::MatrixTransform;
-//	transformation2->setMatrix(osg::Matrix::translate(0.5f, 0.0f, 0.0f));
-//	transformation2->addChild(model.get());
-//
-//	osg::ref_ptr<osg::Group> root = new osg::Group;
-//	root->addChild(transformation1.get());
-//	root->addChild(transformation2.get());
-//
-//	transformation1->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-//	transformation2->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF | osg::StateAttribute::PROTECTED);
-//	root->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-//
-//	osgViewer::Viewer viewer;
-//	viewer.setSceneData(root.get());
-//	return viewer.run();
-//}
+#include <osg/PolygonMode>
+#include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+
+int main(int argc, char** argv)
+{
+	osg::ref_ptr<osg::Node> model = osgDB::readNodeFile("glider.osg");
+
+	osg::ref_ptr<osg::MatrixTransform> transformation1 = new osg::MatrixTransform;
+	transformation1->setMatrix(osg::Matrix::translate(-0.5f, 0.0f, 0.0f));
+	transformation1->addChild(model.get());
+
+	osg::ref_ptr<osg::MatrixTransform> transformation2 = new osg::MatrixTransform;
+	transformation2->setMatrix(osg::Matrix::translate(0.5f, 0.0f, 0.0f));
+	transformation2->addChild(model.get());
+
+	osg::ref_ptr<osg::Group> root = new osg::Group;
+	root->addChild(transformation1.get());
+	root->addChild(transformation2.get());
+
+	transformation1->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF); //lighting is still on since the root is affecting (this setmode to off has no affect whatsoever)
+	transformation2->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF | osg::StateAttribute::PROTECTED); //protected means that it prevents itself from being affected by the root.
+	root->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(root.get());
+	return viewer.run();
+}
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -780,76 +787,76 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //page 158 (geometry shader - generating a Bezier curve)
 
-#include <osg/Program>
-#include <osg/LineWidth>
-#include <osgDB/ReadFile>
-#include <osgViewer/Viewer>
-
-static const char* vertSource = {
-"#version 120\n"
-"#extension GL_EXT_geometry_shader4 : enable\n"
-"void main()\n"
-"{ gl_Position = ftransform(); }\n"
-};
-
-static const char* geomSource = {
-"#version 120\n"
-"#extension GL_EXT_geometry_shader4 : enable\n"
-"uniform int segments;\n"
-"void main(void)\n"
-"{\n"
-" float delta = 1.0 / float(segments);\n"
-" vec4 v;\n"
-" for ( int i=0; i<=segments; ++i )\n"
-" {\n"
-" float t = delta * float(i);\n"
-" float t2 = t * t;\n"
-" float one_minus_t = 1.0 - t;\n"
-" float one_minus_t2 = one_minus_t * one_minus_t;\n"
-" v = gl_PositionIn[0] * one_minus_t2 * one_minus_t + \n"
-" gl_PositionIn[1] * 3.0 * t * one_minus_t2 +\n"
-" gl_PositionIn[2] * 3.0 * t2 * one_minus_t +\n"
-" gl_PositionIn[3] * t2 * t;\n"
-" gl_Position = v;\n"
-" EmitVertex();\n"
-" }\n"
-" EndPrimitive();\n"
-"}\n"
-};
-
-int main(int argc, char** argv)
-{
-	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-	vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
-	vertices->push_back(osg::Vec3(1.0f, 1.0f, 1.0f));
-	vertices->push_back(osg::Vec3(2.0f, 1.0f, -1.0f));
-	vertices->push_back(osg::Vec3(3.0f, 0.0f, 0.0f));
-	osg::ref_ptr<osg::Geometry> controlPoints = new osg::Geometry;
-	controlPoints->setVertexArray(vertices.get());
-	controlPoints->addPrimitiveSet(new osg::DrawArrays(GL_LINES_ADJACENCY_EXT, 0, 4));
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(controlPoints.get());
-	int segments = 10;
-	osg::ref_ptr<osg::Program> program = new osg::Program;
-	program->addShader(new osg::Shader(osg::Shader::VERTEX, vertSource));
-	program->addShader(new osg::Shader(osg::Shader::GEOMETRY, geomSource));
-	program->setParameter(GL_GEOMETRY_VERTICES_OUT_EXT, segments + 1);
-	program->setParameter(GL_GEOMETRY_INPUT_TYPE_EXT, GL_LINES_ADJACENCY_EXT);
-	program->setParameter(GL_GEOMETRY_OUTPUT_TYPE_EXT, GL_LINE_STRIP);
-
-	osg::ref_ptr<osg::LineWidth> lineWidth = new osg::LineWidth;
-	lineWidth->setWidth(7.0f);
-
-	osg::StateSet* stateset = geode->getOrCreateStateSet();
-	stateset->setAttributeAndModes(program.get());
-	stateset->setAttribute(lineWidth.get());
-	stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-	stateset->addUniform(new osg::Uniform("segments", segments));
-
-	osgViewer::Viewer viewer;
-	viewer.setSceneData(geode.get());
-	return viewer.run();
-}
+//#include <osg/Program>
+//#include <osg/LineWidth>
+//#include <osgDB/ReadFile>
+//#include <osgViewer/Viewer>
+//
+//static const char* vertSource = {
+//"#version 120\n"
+//"#extension GL_EXT_geometry_shader4 : enable\n"
+//"void main()\n"
+//"{ gl_Position = ftransform(); }\n"
+//};
+//
+//static const char* geomSource = {
+//"#version 120\n"
+//"#extension GL_EXT_geometry_shader4 : enable\n"
+//"uniform int segments;\n"
+//"void main(void)\n"
+//"{\n"
+//" float delta = 1.0 / float(segments);\n"
+//" vec4 v;\n"
+//" for ( int i=0; i<=segments; ++i )\n"
+//" {\n"
+//" float t = delta * float(i);\n"
+//" float t2 = t * t;\n"
+//" float one_minus_t = 1.0 - t;\n"
+//" float one_minus_t2 = one_minus_t * one_minus_t;\n"
+//" v = gl_PositionIn[0] * one_minus_t2 * one_minus_t + \n"
+//" gl_PositionIn[1] * 3.0 * t * one_minus_t2 +\n"
+//" gl_PositionIn[2] * 3.0 * t2 * one_minus_t +\n"
+//" gl_PositionIn[3] * t2 * t;\n"
+//" gl_Position = v;\n"
+//" EmitVertex();\n"
+//" }\n"
+//" EndPrimitive();\n"
+//"}\n"
+//};
+//
+//int main(int argc, char** argv)
+//{
+//	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
+//	vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
+//	vertices->push_back(osg::Vec3(1.0f, 1.0f, 1.0f));
+//	vertices->push_back(osg::Vec3(2.0f, 1.0f, -1.0f));
+//	vertices->push_back(osg::Vec3(3.0f, 0.0f, 0.0f));
+//	osg::ref_ptr<osg::Geometry> controlPoints = new osg::Geometry;
+//	controlPoints->setVertexArray(vertices.get());
+//	controlPoints->addPrimitiveSet(new osg::DrawArrays(GL_LINES_ADJACENCY_EXT, 0, 4));
+//	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+//	geode->addDrawable(controlPoints.get());
+//	int segments = 10;
+//	osg::ref_ptr<osg::Program> program = new osg::Program;
+//	program->addShader(new osg::Shader(osg::Shader::VERTEX, vertSource));
+//	program->addShader(new osg::Shader(osg::Shader::GEOMETRY, geomSource));
+//	program->setParameter(GL_GEOMETRY_VERTICES_OUT_EXT, segments + 1);
+//	program->setParameter(GL_GEOMETRY_INPUT_TYPE_EXT, GL_LINES_ADJACENCY_EXT);
+//	program->setParameter(GL_GEOMETRY_OUTPUT_TYPE_EXT, GL_LINE_STRIP);
+//
+//	osg::ref_ptr<osg::LineWidth> lineWidth = new osg::LineWidth;
+//	lineWidth->setWidth(7.0f);
+//
+//	osg::StateSet* stateset = geode->getOrCreateStateSet();
+//	stateset->setAttributeAndModes(program.get());
+//	stateset->setAttribute(lineWidth.get());
+//	stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+//	stateset->addUniform(new osg::Uniform("segments", segments));
+//
+//	osgViewer::Viewer viewer;
+//	viewer.setSceneData(geode.get());
+//	return viewer.run();
+//}
 
 
 
