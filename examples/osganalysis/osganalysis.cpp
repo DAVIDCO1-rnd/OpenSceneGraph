7a543712b7ec02a3c1c4ec4e96581a629c67451a
David Cohn <davidco1@rnd-hub.com>
2022-03-21 16:55:09 +0200

    updated

--- examples/osganalysis/osganalysis.cpp
+++ examples/osganalysis/osganalysis.cpp
@@ -1024,63 +1024,74 @@ using namespace std;
 //}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//page 148 (171 of 412)
-
-#include <osg/BlendFunc>
-#include <osg/Texture2D>
-#include <osg/Geometry>
-#include <osgDB/ReadFile>
-#include <osgViewer/Viewer>
-
-int main(int argc, char** argv)
-{
-	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-	vertices->push_back(osg::Vec3(-0.5f, 0.0f, -0.5f));
-	vertices->push_back(osg::Vec3(0.5f, 0.0f, -0.5f));
-	vertices->push_back(osg::Vec3(0.5f, 0.0f, 0.5f));
-	vertices->push_back(osg::Vec3(-0.5f, 0.0f, 0.5f));
-	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
-	normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
-	osg::ref_ptr<osg::Vec2Array> texcoords = new osg::Vec2Array;
-	texcoords->push_back(osg::Vec2(0.0f, 0.0f));
-	texcoords->push_back(osg::Vec2(0.0f, 1.0f));
-	texcoords->push_back(osg::Vec2(1.0f, 1.0f));
-	texcoords->push_back(osg::Vec2(1.0f, 0.0f));
-	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
-	colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 0.5f));
-	osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
-	quad->setVertexArray(vertices.get());
-	quad->setNormalArray(normals.get());
-	quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
-	quad->setColorArray(colors.get());
-	quad->setColorBinding(osg::Geometry::BIND_OVERALL);
-	quad->setTexCoordArray(0, texcoords.get());
-	quad->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(quad.get());
-
-	osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
-	osg::ref_ptr<osg::Image> image = osgDB::readImageFile("Images/lz.rgb");
-	texture->setImage(image.get());
-
-	osg::ref_ptr<osg::BlendFunc> blendFunc = new osg::BlendFunc;
-	blendFunc->setFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	osg::StateSet* stateset = geode->getOrCreateStateSet();
-	stateset->setTextureAttributeAndModes(0, texture.get());
-	stateset->setAttributeAndModes(blendFunc);
-
-	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(geode.get());
-	root->addChild(osgDB::readNodeFile("glider.osg"));
-	osgViewer::Viewer viewer;
-	viewer.setSceneData(root.get());
-	return viewer.run();
-}
+////page 148 (171 of 412)
+//
+//#include <osg/BlendFunc>
+//#include <osg/Texture2D>
+//#include <osg/Geometry>
+//#include <osgDB/ReadFile>
+//#include <osgViewer/Viewer>
+//
+//int main(int argc, char** argv)
+//{
+//	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
+//	vertices->push_back(osg::Vec3(-0.5f, 0.0f, -0.5f));
+//	vertices->push_back(osg::Vec3(0.5f, 0.0f, -0.5f));
+//	vertices->push_back(osg::Vec3(0.5f, 0.0f, 0.5f));
+//	vertices->push_back(osg::Vec3(-0.5f, 0.0f, 0.5f));
+//	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+//	normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
+//	osg::ref_ptr<osg::Vec2Array> texcoords = new osg::Vec2Array;
+//	texcoords->push_back(osg::Vec2(0.0f, 0.0f));
+//	texcoords->push_back(osg::Vec2(0.0f, 1.0f));
+//	texcoords->push_back(osg::Vec2(1.0f, 1.0f));
+//	texcoords->push_back(osg::Vec2(1.0f, 0.0f));
+//	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
+//	colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 0.5f));
+//	osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
+//	quad->setVertexArray(vertices.get());
+//	quad->setNormalArray(normals.get());
+//	quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
+//	quad->setColorArray(colors.get());
+//	quad->setColorBinding(osg::Geometry::BIND_OVERALL);
+//	quad->setTexCoordArray(0, texcoords.get());
+//	quad->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
+//	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+//	geode->addDrawable(quad.get());
+//
+//	osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
+//	osg::ref_ptr<osg::Image> image = osgDB::readImageFile("Images/lz.rgb");
+//	texture->setImage(image.get());
+//
+//	osg::ref_ptr<osg::BlendFunc> blendFunc = new osg::BlendFunc;
+//	blendFunc->setFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//	
+//
+//	osg::StateSet* stateset = geode->getOrCreateStateSet();
+//	stateset->setTextureAttributeAndModes(0, texture.get());
+//	stateset->setAttributeAndModes(blendFunc);
+//
+//	/*
+//	page 147 (170 of 412)
+//	The setRenderingHint() method of the osg::StateSet class will tell OSG to control
+//the rendering order of nodes and drawables if necessary. It simply indicates whether a state
+//set is opaque or transparent, and ensures that objects associated with transparent states
+//should be rendered after opaque ones, and these transparent objects should be sorted by
+//the length from every object's center to the eye position (that is, from a distance to nearby).
+//	*/
+//	stateset->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+//
+//	osg::ref_ptr<osg::Group> root = new osg::Group;
+//	root->addChild(geode.get());
+//	root->addChild(osgDB::readNodeFile("glider.osg"));
+//	osgViewer::Viewer viewer;
+//	viewer.setSceneData(root.get());
+//	return viewer.run();
+//}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//page 154 (shaders)
-
+////page 154 (177 of 412)
+//
 //#include <osg/Program>
 //#include <osgDB/ReadFile>
 //#include <osgViewer/Viewer>
@@ -1134,76 +1145,61 @@ int main(int argc, char** argv)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //page 158 (geometry shader - generating a Bezier curve)
 
-//#include <osg/Program>
-//#include <osg/LineWidth>
-//#include <osgDB/ReadFile>
-//#include <osgViewer/Viewer>
-//
-//static const char* vertSource = {
-//"#version 120\n"
-//"#extension GL_EXT_geometry_shader4 : enable\n"
-//"void main()\n"
-//"{ gl_Position = ftransform(); }\n"
-//};
-//
-//static const char* geomSource = {
-//"#version 120\n"
-//"#extension GL_EXT_geometry_shader4 : enable\n"
-//"uniform int segments;\n"
-//"void main(void)\n"
-//"{\n"
-//" float delta = 1.0 / float(segments);\n"
-//" vec4 v;\n"
-//" for ( int i=0; i<=segments; ++i )\n"
-//" {\n"
-//" float t = delta * float(i);\n"
-//" float t2 = t * t;\n"
-//" float one_minus_t = 1.0 - t;\n"
-//" float one_minus_t2 = one_minus_t * one_minus_t;\n"
-//" v = gl_PositionIn[0] * one_minus_t2 * one_minus_t + \n"
-//" gl_PositionIn[1] * 3.0 * t * one_minus_t2 +\n"
-//" gl_PositionIn[2] * 3.0 * t2 * one_minus_t +\n"
-//" gl_PositionIn[3] * t2 * t;\n"
-//" gl_Position = v;\n"
-//" EmitVertex();\n"
-//" }\n"
-//" EndPrimitive();\n"
-//"}\n"
-//};
-//
-//int main(int argc, char** argv)
-//{
-//	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-//	vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
-//	vertices->push_back(osg::Vec3(1.0f, 1.0f, 1.0f));
-//	vertices->push_back(osg::Vec3(2.0f, 1.0f, -1.0f));
-//	vertices->push_back(osg::Vec3(3.0f, 0.0f, 0.0f));
-//	osg::ref_ptr<osg::Geometry> controlPoints = new osg::Geometry;
-//	controlPoints->setVertexArray(vertices.get());
-//	controlPoints->addPrimitiveSet(new osg::DrawArrays(GL_LINES_ADJACENCY_EXT, 0, 4));
-//	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-//	geode->addDrawable(controlPoints.get());
-//	int segments = 10;
-//	osg::ref_ptr<osg::Program> program = new osg::Program;
-//	program->addShader(new osg::Shader(osg::Shader::VERTEX, vertSource));
-//	program->addShader(new osg::Shader(osg::Shader::GEOMETRY, geomSource));
-//	program->setParameter(GL_GEOMETRY_VERTICES_OUT_EXT, segments + 1);
-//	program->setParameter(GL_GEOMETRY_INPUT_TYPE_EXT, GL_LINES_ADJACENCY_EXT);
-//	program->setParameter(GL_GEOMETRY_OUTPUT_TYPE_EXT, GL_LINE_STRIP);
-//
-//	osg::ref_ptr<osg::LineWidth> lineWidth = new osg::LineWidth;
-//	lineWidth->setWidth(7.0f);
-//
-//	osg::StateSet* stateset = geode->getOrCreateStateSet();
-//	stateset->setAttributeAndModes(program.get());
-//	stateset->setAttribute(lineWidth.get());
-//	stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-//	stateset->addUniform(new osg::Uniform("segments", segments));
-//
-//	osgViewer::Viewer viewer;
-//	viewer.setSceneData(geode.get());
-//	return viewer.run();
-//}
+#include <osg/Program>
+#include <osg/LineWidth>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+
+int main(int argc, char** argv)
+{
+	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
+	vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
+	vertices->push_back(osg::Vec3(0.0f, 0.0f, 1.0f));
+	vertices->push_back(osg::Vec3(1.0f, 0.0f, 1.0f));
+	vertices->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
+	osg::ref_ptr<osg::Geometry> controlPoints = new osg::Geometry;
+	controlPoints->setVertexArray(vertices.get());
+	controlPoints->addPrimitiveSet(new osg::DrawArrays(GL_LINES_ADJACENCY_EXT, 0, 4));
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(controlPoints.get());
+	int segments = 3;
+	osg::ref_ptr<osg::Program> program = new osg::Program;
+
+	std::string shaderFolder = "D:/Developments/OSG_folders/OpenSceneGraph/examples/osganalysis";
+	//std::string shaderFolder = ".";
+	std::string vertexShaderPath = shaderFolder + "/" + "shader.vert";
+	std::string geometryShaderPath = shaderFolder + "/" + "shader.geom";
+
+	osg::ref_ptr<osg::Shader> vertexShader = new osg::Shader(osg::Shader::VERTEX);
+	vertexShader->loadShaderSourceFromFile(vertexShaderPath);
+	program->addShader(vertexShader);
+
+	osg::ref_ptr<osg::Shader> geomShader = new osg::Shader(osg::Shader::GEOMETRY);
+	geomShader->loadShaderSourceFromFile(geometryShaderPath);
+	program->addShader(geomShader);
+
+	//program->addShader(new osg::Shader(osg::Shader::VERTEX, vertSource));
+	//program->addShader(new osg::Shader(osg::Shader::GEOMETRY, geomSource));
+
+
+
+	program->setParameter(GL_GEOMETRY_VERTICES_OUT_EXT, segments + 1);
+	program->setParameter(GL_GEOMETRY_INPUT_TYPE_EXT, GL_LINES_ADJACENCY_EXT);
+	program->setParameter(GL_GEOMETRY_OUTPUT_TYPE_EXT, GL_LINE_STRIP);
+
+	osg::ref_ptr<osg::LineWidth> lineWidth = new osg::LineWidth;
+	lineWidth->setWidth(7.0f);
+
+	osg::StateSet* stateset = geode->getOrCreateStateSet();
+	stateset->setAttributeAndModes(program.get());
+	stateset->setAttribute(lineWidth.get());
+	stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+	stateset->addUniform(new osg::Uniform("segments", segments));
+
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(geode.get());
+	return viewer.run();
+}
 
 
 
